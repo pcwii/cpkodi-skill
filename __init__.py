18e4b95c2bfb86b4787f9fa9bd567c5e7c42f33f
philc <philc@arcx.com>
2020-06-23 22:41:22 -0400

    updated to check for artist when specifying song or album
    Change-Id: I43f6324e0ed22e1dc751b184e9dfb34d8372b458

--- __init__.py
+++ __init__.py
@@ -217,7 +217,7 @@ class CPKodiSkill(CommonPlaySkill):
             LOG.info('Album Type')
             request_type = 'album'
             request_item = album_type.groupdict()['album']
-            artist_specified = re.match(self.translate_regex('artist.name'), phrase)
+            artist_specified = re.match(self.translate_regex('artist.name'), str(request_item))
             if artist_specified:
                 LOG.info('Artist also specified')
                 artist_name = artist_specified.groupdict()['artist_Name']
@@ -238,7 +238,7 @@ class CPKodiSkill(CommonPlaySkill):
             LOG.info('Song Type')
             request_type = 'title'
             request_item = song_type.groupdict()['title']
-            artist_specified = re.match(self.translate_regex('artist.name'), phrase)
+            artist_specified = re.match(self.translate_regex('artist.name'), str(request_item))
             if artist_specified:
                 LOG.info('Artist also specified')
                 artist_name = artist_specified.groupdict()['artist_Name']
