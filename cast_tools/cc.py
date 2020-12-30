816388f1f27456abf7d9eedfb6585d43d80701f2
Phil Chenier <philc@arcx.com>
2020-12-30 16:32:50 -0500

    updated cc_get_names()
    Change-Id: I33eb081f5fd0620a12593a7ea89c0e560fdf5013

--- cast_tools/cc.py
+++ cast_tools/cc.py
@@ -2,8 +2,8 @@ import pychromecast
 #myFile = "http://192.168.0.32:8080/vfs/%2Fhome%2Fosmc%2Fmnt%2FnfsMovies%2FMarvel%2FSpider-Man%2FSpider.Man.2.2004.1080p.mp4"
 
 def cc_get_names():
-    casts, browser = pychromecast.get_chromecasts()
-    pychromecast.discovery.stop_discovery(browser)
+    casts = pychromecast.get_chromecasts()
+    #pychromecast.discovery.stop_discovery(browser)
     deviceList = []
     if len(casts) == 0:
         return None
