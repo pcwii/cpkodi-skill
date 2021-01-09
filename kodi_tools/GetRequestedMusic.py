bf631555ec8164d84cc79d6424785f0d66fcd67f
Phil Chenier <philc@arcx.com>
2021-01-08 20:26:19 -0500

    added trap for no songs returned in GetRequestedMusic.py
    Change-Id: Ibdbc46010dec40d46f124ecde14109737538c179

--- kodi_tools/GetRequestedMusic.py
+++ kodi_tools/GetRequestedMusic.py
@@ -63,24 +63,29 @@ def get_requested_music(kodi_path, search_item, search_type):
         # LOG.info("payload: " + str(kodi_payload))
         kodi_response = requests.post(api_path, data=json.dumps(kodi_payload), headers=json_header)
         LOG.info("Music json" + kodi_response.text)
-        song_list = json.loads(kodi_response.text)['result']['songs']
-        # remove duplicates
-        clean_list = []  # this is a dict
-        for each_song in song_list:
-            song_title = str(each_song['label'])
-            info = {
-                "label": each_song['label'],
-                "songid": each_song['songid'],
-                "artist": each_song['artist']
-            }
-            if song_title.lower() not in str(clean_list).lower():
-                clean_list.append(info)
-            else:
-                if len(each_song['label']) == len(song_title):
-                    LOG.info('Removing Duplicate Entries')
-                else:
+        item_count = int(json.loads(kodi_response.text)['result']['limits']['total'])
+        if item_count > 0:
+            song_list = json.loads(kodi_response.text)['result']['songs']
+            # remove duplicates
+            clean_list = []  # this is a dict
+            for each_song in song_list:
+                song_title = str(each_song['label'])
+                info = {
+                    "label": each_song['label'],
+                    "songid": each_song['songid'],
+                    "artist": each_song['artist']
+                }
+                if song_title.lower() not in str(clean_list).lower():
                     clean_list.append(info)
-        return clean_list  # returns a dictionary of matched movies
+                else:
+                    if len(each_song['label']) == len(song_title):
+                        LOG.info('Removing Duplicate Entries')
+                    else:
+                        clean_list.append(info)
+            return clean_list  # returns a dictionary of matched movies
+        else:
+            LOG.info('No Songs Found! - ' + str(kodi_payload))
+            return None
 #    except Exception as e:
 #        LOG.info(e)
 #        return None
