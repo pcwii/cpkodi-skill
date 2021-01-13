935a4a505f596b7152ef1dcced79d5cb79a49c9a
Phil Chenier <philc@arcx.com>
2021-01-12 21:21:04 -0500

    updated GetRequestedTVShows.py
    Change-Id: I8b308f74e9e849fa03822c4426a8ee72a444e82f

--- kodi_tools/GetRequestedTVShows.py
+++ kodi_tools/GetRequestedTVShows.py
@@ -110,7 +110,7 @@ def get_episode(api_path, show_id, show_data):
         }
     }
     kodi_response = requests.post(api_path, data=json.dumps(kodi_payload), headers=json_header)
-    item_list = json.loads(kodi_response.text)["result"]["episodes"]
+    item_list = json.loads(kodi_response.text)
     LOG.info(item_list)
     for each_item in item_list:
         if each_item["episode"] == show_data['episode']:
